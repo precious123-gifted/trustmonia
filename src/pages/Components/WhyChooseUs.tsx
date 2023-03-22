1ab577ddc721efb4cb2af96dfa0361878f685b1c
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-03-22 15:54:17 +0100

    Mar 22 15:54

--- src/pages/Components/WhyChooseUs.tsx
+++ src/pages/Components/WhyChooseUs.tsx
@@ -6,22 +6,22 @@ import Image from 'next/image'
 
 export default function WhyChooseUs() {
   return (
-    <div className='w-[100vw] h-[200vw] portrait:h-[500vw] bg-grey flex item-center justify-center pt-[6%] portrait:pt-[30%]'>
+    <div className='w-[100vw] h-[200vw] portrait:h-[500vw] bg-grey flex item-center justify-center pt-[6vw]  portrait:pt-[30vw] portrait:sm:pt-[20vw] '>
       <div className="content flex-col justify-between align-center h-full w-[95%]  ">
-<div className="header text-[200%]  portrait:text-[150%] text-center mb-[13%] portrait:mb-[30%]">WHY CHOOSE TRUSTMONIA</div>
+<div className="header text-[2.5vw]  portrait:text-[7vw]  portrait:sm:text-[5vw] text-center mb-[13%] portrait:mb-[30%]">WHY CHOOSE TRUSTMONIA</div>
 
  
 
 <div className="section1 flex items-center justify-between mb-[13%] portrait:mb-[25%]">
 <div className="svg  portrait:hidden ">
-<Image alt='' src={speakerSVG}  className='w-[100%] object-contain aspect-[4/3]  landscape:object-left ' />
+<Image alt='' src={speakerSVG}  className='w-[45vw] object-contain aspect-[4/3]  landscape:object-left ' />
 </div>
 
 
 <div className="writeup-div w-[40%]  portrait:w-[100%] bg-pink rounded p-[3%] portrait:p-[7%] border-2 border-darkgreen">
-<div className="writeup-header text-[170%] portrait:text-center portrait:text-[140%] mb-[4%]">We Speak for Businesses</div>
+<div className="writeup-header  text-[2vw]  portrait:text-[6vw]  portrait:sm:text-[4vw]  portrait:text-center  mb-[4%]">We Speak for Businesses</div>
 
-<div className="writeup text-[130%]  portrait:text-center portrait:text-[142%] font-manuale">
+<div className="writeup  text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw] portrait:text-center  font-manuale">
 Trustmonia is a review platform
 that’s open to everyone. Share
 your experiences to help others
@@ -46,9 +46,9 @@ their game.
 
 
 <div className="writeup-div w-[40%]  portrait:w-[100%] bg-blue rounded p-[3%] portrait:p-[7%] border-2 border-darkgreen">
-<div className="writeup-header text-[170%] portrait:text-center portrait:text-[140%] mb-[4%]">We Offer Service Review</div>
+<div className="writeup-header text-[2.5vw]  portrait:text-[7vw]  portrait:sm:text-[5vw] portrait:text-center  mb-[4%]">We Offer Service Review</div>
 
-<div className="writeup text-[130%]  portrait:text-center portrait:text-[142%] font-manuale">
+<div className="writeup text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]  portrait:text-center font-manuale">
 Trustmonia is a review platform
 that’s open to everyone. Share
 your experiences to help others
@@ -65,7 +65,7 @@ their game.
 
 
 <div className="svg  portrait:hidden ">
-<Image alt='' src={serviceSVG}  className='w-[100%] object-contain aspect-[4/3]  landscape:object-right ' />
+<Image alt='' src={serviceSVG}  className='w-[45vw] object-contain aspect-[4/3]  landscape:object-right ' />
 </div>
 
 </div>
@@ -75,12 +75,14 @@ their game.
 
 <div className="section3 flex items-center justify-between mb-[13%] portrait:mb-[25%]">
 
-
+<div className="svg  portrait:hidden ">
+<Image alt='' src={productSVG}  className='w-[45vw] object-contain aspect-[4/3]  landscape:object-left ' />
+</div>
 
 <div className="writeup-div w-[40%]  portrait:w-[100%] bg-green rounded p-[3%] portrait:p-[7%] border-2 border-darkgreen">
 <div className="writeup-header text-[170%] portrait:text-center portrait:text-[140%] mb-[4%]">We Offer Service Review</div>
 
-<div className="writeup text-[130%]  portrait:text-center portrait:text-[142%] font-manuale">
+<div className="writeup text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]  portrait:text-center font-manuale">
 Trustmonia is a review platform
 that’s open to everyone. Share
 your experiences to help others
@@ -96,9 +98,7 @@ their game.
 
 
 
-<div className="svg  portrait:hidden ">
-<Image alt='' src={productSVG}  className='w-[100%] object-contain aspect-[4/3]  landscape:object-right ' />
-</div>
+
 
 </div>
 
