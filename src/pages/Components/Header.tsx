63fb245cd93140c19b141c198297b5672962295f
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-03-22 13:36:55 +0100

    Mar 22 13:36

--- src/pages/Components/Header.tsx
+++ src/pages/Components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 
         <div className="content  w-[95%] pt-[5%] h-full portrait:flex-col landscape:flex landscape:items-start landscape:justify-between">
 <div className="section1 portrait:mb-[10%] font-meduim font-loma landscape:w-[40%] landscape:flex-col landscape:justify-between">
-<div className="headerTXT   portrait:text-[200%]  font-[510] text-darkgreen  landscape:text-[4vw] landscape:h-[70%]  landscape:mb-[9%]"><span>CONNECTING PEOPLE
+<div className="headerTXT   portrait:text-[10vw]  font-[510] text-darkgreen  landscape:text-[4vw] landscape:h-[70%]  landscape:mb-[9%]"><span>CONNECTING PEOPLE
 WITH COMPANIES
 THEY CAN TRUST</span> </div>
 
@@ -21,11 +21,11 @@ THEY CAN TRUST</span> </div>
 </div>
 
 <div className="section2   lanscape:w-[40%]  landscape:flex landscape:items-start landscape:justify-end ">
-<Image  alt='' src={headerIMG}   className='w-[50vw]    object-contain aspect-[4/3] landscape:object-right portrait:mb-[17%]  portrait:w-[150%] ' />
+<Image  alt='' src={headerIMG}   className='w-[50vw]    object-contain aspect-[4/3] landscape:object-right portrait:mb-[17%]  portrait:w-[100vw] ' />
 
 
-<div className="BTN text-[4vw] landscape:hidden flex item-center justify-center">
-    <span className=' transition-all ease-in duration-[0.5s] bg-armygreen hover:bg-blue hover:text-armygreen p-[1vw] text-grey rounded cursor-pointer'>Company Review</span>
+<div className="BTN landscape:text-[4vw]  portrait:text-[7vw] landscape:hidden flex item-center justify-center">
+    <span className=' transition-all ease-in duration-[0.5s] bg-armygreen hover:bg-blue hover:text-armygreen p-[1vw] portrait:p-[3.5vw] text-grey rounded cursor-pointer'>Company Review</span>
 </div>
 </div>
 
