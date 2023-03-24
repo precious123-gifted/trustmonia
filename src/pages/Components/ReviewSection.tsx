5c7356f5fe2b0f69a79a3473996a30864682e71a
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-03-24 13:01:49 +0100

    Mar 24 13:01

---
+++ src/pages/Components/ReviewSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ReviewSection() {
+  return (
+    <div className=''>
+      <div className="content">
+
+        
+      </div>
+    </div>
+  )
+}
