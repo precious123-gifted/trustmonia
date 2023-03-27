c11b6b1cf10716b3a82ddf8b31c0894d4c349c52
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-03-27 16:31:57 +0100

    Mar 27 16:31

--- src/pages/Components/ReviewSection.tsx
+++ src/pages/Components/ReviewSection.tsx
@@ -16,13 +16,13 @@ export default function ReviewSection() {
 </div>
 
 
-<div className="cards-container text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw] font-manuale overflow-x-hidden w-full h-[70%] portrait:h-[50%]  portrait:sm:h-[50%] portrait:w-[100%]  flex justify-center portrait:justify-center items-center">
+<div className="cards-container text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw] font-manuale overflow-x-hidden w-full h-[70%] portrait:h-[50%]  portrait:sm:h-[45%] portrait:w-[100%]  flex justify-center portrait:justify-center items-center">
 
     { <div className="left-arrow landscape:hidden"><Image  className=' cursor-pointer   portrait:w-[9vw]  portrait:sm:w-[6vw] object-contain aspect-[4/3]' src={leftArrow}  alt='' /></div>
  }
 <div className="cards  w-[80%] overflow-x-auto scrollbar-hide portrait:w-[100%] h-full flex  justify-between items-center">
 
-    <div className="card h-[90%] w-[30%] portrait:h-[80%] portrait:sm:h-[100%] portrait:min-w-[85vw] portrait:sm:min-w-[101%]  bg-blue grid place-items-center">
+    <div className="card  portrait:mr-[5vw] portrait:sm:mr[2vw] h-[90%] w-[30%] portrait:h-[80%] portrait:sm:h-[100%] portrait:min-w-[85vw] portrait:sm:min-w-[101%]  bg-blue grid place-items-center">
 
 <div className="section1 h-[40%]  w-[90%]   "><Image alt='' src={stars}/></div>
 <div className="section2 h-[100%] w-[90%]  text-center ">They have one of the best customer
@@ -33,7 +33,7 @@ my money</div>
 
     </div>
 
-    <div className="card h-[90%] w-[30%]   portrait:h-[80%] portrait:sm:h-[100%] portrait:min-w-[85vw] portrait:sm:min-w-[101%]  bg-blue grid place-items-center">
+    <div className="card  portrait:mr-[5vw] portrait:sm:mr[2vw]  h-[90%] w-[30%]   portrait:h-[80%] portrait:sm:h-[100%] portrait:min-w-[85vw] portrait:sm:min-w-[101%]  bg-blue grid place-items-center">
 
     <div className="section1 h-[40%]  w-[90%]   "><Image alt='' src={stars}/></div>
 <div className="section2 h-[100%] w-[90%]  ">They have one of the best customer
