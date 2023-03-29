641748632cbc2fb88d0f57dbfc732cfabfdc5144
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-03-29 09:01:28 +0100

    Mar 29 09:01

--- src/pages/Components/Footer.tsx
+++ src/pages/Components/Footer.tsx
@@ -2,9 +2,55 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='landscape:h-[30vw] portrait:h-[190vw]  w-[100vw] bg-blue  flex items-start justify-center'>
-      <div className="content h-full">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ipsum ad alias. Consequuntur officiis eius deserunt, repellendus vel id sapiente, unde voluptas odio labore quasi quia beatae repudiandae, doloribus a.
+    <div className='landscape:h-[30vw] portrait:h-[190vw] pt-[5%] w-[100vw] bg-blue  flex items-start justify-center'>
+      <div className="content w-[95%] h-full flex-col">
+     <div className="details w-full h-[90%] flex justify-between portrait:flex-col">
+<div className="section1 w-[45%] portrait:sm:flex portrait:flex-col portrait:w-full flex justify-between">
+  <div className="block1">
+     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
+    <div className="list">TreNova Academy is
+owned by TreNova Group</div>
+   
+  </div> 
+  </div>
+  <div className="block2">
+     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
+    <div className="list">as</div>
+    <div className="list">asas</div>
+    <div className="list">fesd</div>
+    <div className="list">sdsld</div>
+  </div> 
+  </div>
+</div>
+<div className="section2 w-[45%]  portrait:sm:flex-row portrait:flex-col  portrait:w-full flex justify-between">
+<div className="block1">
+     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
+    <div className="list">as</div>
+    <div className="list">asas</div>
+    <div className="list">fesd</div>
+    <div className="list">sdsld</div>
+  </div> 
+  </div>
+  <div className="block2">
+     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
+    <div className="list">as</div>
+    <div className="list">asas</div>
+    <div className="list">fesd</div>
+    <div className="list">sdsld</div>
+  </div> 
+  </div>
+
+</div>
+
+     </div>
+
+<div className="foot">
+
+</div>
       </div>
     </div>
   )
