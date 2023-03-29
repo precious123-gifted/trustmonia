43fa387c968222b58cbd094346ad6bd983aee40c
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-03-29 09:36:07 +0100

    Mar 29 09:36

--- src/pages/Components/Footer.tsx
+++ src/pages/Components/Footer.tsx
@@ -2,45 +2,47 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='landscape:h-[30vw] portrait:h-[190vw] pt-[5%] w-[100vw] bg-blue  flex items-start justify-center'>
+    <div className='landscape:h-[18vw] portrait:h-[140vw] pt-[5%] w-[100vw] bg-blue  flex items-start justify-center'>
       <div className="content w-[95%] h-full flex-col">
      <div className="details w-full h-[90%] flex justify-between portrait:flex-col">
-<div className="section1 w-[45%] portrait:sm:flex portrait:flex-col portrait:w-full flex justify-between">
-  <div className="block1">
-     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
-  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
+<div className="section1 portrait:h-[40%]  w-[45%] portrait:sm:flex portrait:flex-col portrait:w-full flex justify-between">
+  <div className="block1 w-[40%] portrait:w-full  ">
+     <div className="title text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[100%]">
     <div className="list">TreNova Academy is
 owned by TreNova Group</div>
    
   </div> 
   </div>
-  <div className="block2">
-     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
-  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
-    <div className="list">as</div>
-    <div className="list">asas</div>
-    <div className="list">fesd</div>
-    <div className="list">sdsld</div>
+  <div className="block2 w-[40%] portrait:w-full  ">
+     <div className="title text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">COMPANY INFO</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[100%]">
+    <div className="list">About Us</div>
+    <div className="list">Contacts</div>
+    <div className="list">Earn While you Learn</div>
+    <div className="list">Business Opportunities</div>
   </div> 
   </div>
 </div>
-<div className="section2 w-[45%]  portrait:sm:flex-row portrait:flex-col  portrait:w-full flex justify-between">
-<div className="block1">
-     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
-  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
-    <div className="list">as</div>
-    <div className="list">asas</div>
-    <div className="list">fesd</div>
-    <div className="list">sdsld</div>
+<div className="section2 portrait:h-[55%]  w-[45%]  portrait:sm:flex-row portrait:flex-col  portrait:w-full flex justify-between">
+<div className="block1 w-[40%] portrait:w-full  ">
+     <div className="title text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">OTHER LINKS</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[100%]">
+    <div className="list">Trenova Groups</div>
+    <div className="list">Trenova Media</div>
+    <div className="list">School Support Center</div>
+    <div className="list">E-Library</div>
+    <div className="list">Parent Loyalty Platform</div>
   </div> 
   </div>
-  <div className="block2">
-     <div className="title text-[2vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
-  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[50%]">
-    <div className="list">as</div>
-    <div className="list">asas</div>
-    <div className="list">fesd</div>
-    <div className="list">sdsld</div>
+  <div className="block2 w-[40%] portrait:w-full  ">
+     <div className="title text-[1.5vw]  portrait:text-[5vw]  portrait:sm:text-[3vw]">TRENOVA GROUP</div>
+  <div className="lists  text-[1.2vw]  portrait:text-[4vw]  portrait:sm:text-[2.5vw]  w-[100%]">
+    <div className="list">Phone: +234 817 936 1381</div>
+    <div className="list">Email: info@trenovaacademy.com</div>
+    <div className="list">Address: 7B Ogunsiji Close, Allen
+Ikeja, Lagos</div>
+    
   </div> 
   </div>
 
